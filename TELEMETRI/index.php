cc4f6355232ace81859596d4cbf1953b1fac5bef
Habil <151376264+habilaf@users.noreply.github.com>
2023-11-18 20:01:35 +0700

    Add files via upload

---
+++ TELEMETRI/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RUMAH BIBIT UNISMA BEKASI</title>
+    <link rel="stylesheet" href="stylephp.css">
+    <style>
+        body {
+        background-image: url('a.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;  
+        background-size: cover;
+        background-position: center;
+    }
+</style>  
+</head>
+<body>
+    <div>
+    <div>
+    <a>
+      <img class="kop_logo" src="unisma.png" alt="Logo" width="100px" height="100px">
+      <h1 class="judul_prepare">RUMAH BIBIT UNISMA BEKASI</h1>
+    </a>
+    </div>
+    </div>
+        <div class="sedikit_kata">
+            <h2>Silahkan mengisi nama pengguna dan kata kunci</h2>
+            <div>
+                <form>
+                    <input class="form-control" placeholder="Nama Pengguna"></input><br>
+                    <input class="form-control1" placeholder="Kata Kunci"></input><br>
+                    <input class="form-control2" type="submit" value="Masuk">
+                </form>
+                <br>
+            </div>
+            <br>
+            <div style="text-align:center;">
+                 <h3>Hidroponik Sehat dan Segar Tanpa Pestisida</h3>
+               </div>
+        </div>
+    </div>
+
+    
+</body>
+</html>
